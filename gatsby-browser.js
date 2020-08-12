04abbe50150cba988d62c3731756f89ba4842cad
Nevers <livelyevers@gmail.com>
2020-08-12 11:23:17 -0700

    add service worker update function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,15 @@ import { registerLinkResolver } from "gatsby-source-prismic-graphql";
 import linkResolver from "./src/utils/linkResolver";
 
 registerLinkResolver(linkResolver);
+
+// auto-reload when new content available
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
